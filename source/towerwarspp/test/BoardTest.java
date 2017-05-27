4271875e35cc15bb481720a466ec3ddef1871889
Dominick Leppich <dominick.leppich@gmail.com>
2017-05-27 15:31:51 +0200

    Human playable test class

--- source/towerwarspp/test/BoardTest.java
+++ source/towerwarspp/test/BoardTest.java
@@ -3,6 +3,10 @@ package towerwarspp.test;
 import towerwarspp.board.Board;
 import towerwarspp.preset.Move;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
 import java.util.Iterator;
 import java.util.Random;
 import java.util.Set;
@@ -22,20 +26,28 @@ public class BoardTest {
     // ------------------------------------------------------------
 
     public static void main(String[] args) throws InterruptedException {
-        Board b = new Board(10, 3);
+        BufferedReader rd = new BufferedReader(new InputStreamReader(System.in));
+
+        Board b = new Board(5, 5);
 
         System.out.println(b);
         int index = 0;
         boolean red = true;
 
         while (b.getStatus().isOK()) {
-            Thread.sleep(50);
+            //Thread.sleep(50);
 
             System.out.println("------------------------------------------------------------");
-
-            Move m = getRandomMoveFromSet(b.getPossibleMoves());
-
             System.out.println("It's " + (red ? "reds" : "blues") + " turn...");
+
+            Move m = null;
+            try {
+                m = Move.parseMove(rd.readLine());
+            } catch (IOException | Move.MoveFormatException e) {
+                m = getRandomMoveFromSet(b.getPossibleMoves());
+            }
+
+
             System.out.println("Do move No " + ++index + ": " + m + " " + (b.makeMove(m) ? "succeeded" : "failed"));
 
             System.out.println(b);
