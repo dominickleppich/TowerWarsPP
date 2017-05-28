e7daf6c341f1e7b699ea095365558fd15ef0ae1f
Dominick Leppich <dominick.leppich@gmail.com>
2017-05-28 15:25:24 +0200

    Introduce new Player classes

---
+++ source/towerwarspp/player/AbstractPlayer.java
@@ -0,0 +1,9 @@
+package towerwarspp.player;
+
+/**
+ * Created on 28.05.2017.
+ *
+ * @author dominick
+ */
+public class AbstractPlayer {
+}
---
+++ source/towerwarspp/player/ai/Random.java
@@ -0,0 +1,9 @@
+package towerwarspp.player.ai;
+
+/**
+ * Created on 28.05.2017.
+ *
+ * @author dominick
+ */
+public class Random {
+}
