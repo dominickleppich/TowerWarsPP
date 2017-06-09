dadcd558a86c2016c79b36b884211179bdead0c9
Dominick Leppich <dominick.leppich@gmail.com>
2017-06-09 14:56:25 +0200

    [+] Testclass using command line arguments

---
+++ source/towerwarspp/test/StartTest.java
@@ -0,0 +1,57 @@
+package towerwarspp.test;
+
+import towerwarspp.game.Match;
+import towerwarspp.input.Requestable;
+import towerwarspp.input.TextIO;
+import towerwarspp.player.HumanPlayer;
+import towerwarspp.player.ai.RandomAI;
+import towerwarspp.preset.ArgumentParser;
+import towerwarspp.preset.Player;
+import towerwarspp.preset.PlayerType;
+
+import java.util.Observer;
+
+/**
+ * Created on 09.06.2017.
+ *
+ * @author dominick
+ */
+public class StartTest {
+    public static void main(String[] args) throws Exception {
+        // Parse arguments
+        ArgumentParser ap = new ArgumentParser(args);
+
+        // Create requestable and observer
+        TextIO t = new TextIO();
+        Requestable requestable = t;
+        Observer observer = t;
+
+        // Create players
+        Player red, blue;
+        red = createPlayer(ap.getRed(), requestable);
+        blue = createPlayer(ap.getBlue(), requestable);
+
+        // Create match
+        Match match = new Match(red, blue, ap.getSize(), observer);
+        match.init();
+
+        // Start match
+        match.start();
+        match.waitMatch();
+    }
+
+    // ------------------------------------------------------------
+
+    private static Player createPlayer(PlayerType type, Requestable requestable) {
+        switch (type) {
+            case HUMAN:
+               return new HumanPlayer(requestable);
+            case RANDOM_AI:
+                return new RandomAI();
+            case SIMPLE_AI:
+                // TODO implement this
+                return null;
+        }
+        return null;
+    }
+}
