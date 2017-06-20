20a9c62c97ef7fc5b39baa5b1888a49656c003a3
Dominick Leppich <dominick.leppich@gmail.com>
2017-06-20 22:12:18 +0200

    [~] graphic size change

--- source/towerwarspp/io/graphic/java2d/DrawPanel.java
+++ source/towerwarspp/io/graphic/java2d/DrawPanel.java
@@ -25,9 +25,9 @@ import java.util.Map;
  * @author Dominick Leppich
  */
 public class DrawPanel extends JPanel implements MouseListener {
-    private static final int HEX_SIZE = 60;
+    private static final int HEX_SIZE = 50;
     private static final double BASE_SIZE = 0.8;
-    private static final int TOKEN_SIZE = 50;
+    private static final int TOKEN_SIZE = 45;
     private static final int TOWER_RING_DISTANCE = 4;
     private static final float STROKE = 2f;
     private static final Font LABEL_FONT = new Font(Font.SANS_SERIF, Font.BOLD, 10);
